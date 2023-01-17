45c53c05ca1b307f5a29d47041e3ed7b7df150c7
Dmitry Volodin <dmvolod@gmail.com>
2023-01-17 19:32:27 +0300

    Unwrap exec.ExitError in shellcmd.Output and add tests

---
+++ shellcmd/shellcmd_test.go
@@ -0,0 +1,40 @@
+package shellcmd
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/matryer/is"
+)
+
+func TestCommandOutput(t *testing.T) {
+	tests := map[string]struct {
+		command  string
+		expected string
+	}{
+		"success command": {
+			command:  "go version",
+			expected: "",
+		},
+		"unexpected command": {
+			command:  "go1",
+			expected: "executable file not found in $PATH",
+		},
+		"incorrect command": {
+			command:  "go dd",
+			expected: "go dd: unknown command",
+		},
+	}
+
+	for cmd, tt := range tests {
+		t.Run(cmd, func(t *testing.T) {
+			is := is.New(t)
+			_, err := Command(tt.command).Output()
+			if tt.expected == "" {
+				is.NoErr(err)
+			} else {
+				is.True(strings.Contains(err.Error(), tt.expected))
+			}
+		})
+	}
+}
