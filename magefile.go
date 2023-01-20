3ca714cb14afa9344296d1c49911dfe81e09b32e
Dmitry Volodin <dmvolod@gmail.com>
2023-01-20 18:13:30 +0300

    Install mage binary from magefile itself

--- magefile.go
+++ magefile.go
@@ -19,7 +19,31 @@ var (
 		"0.4.1",
 		"https://github.com/princjef/gomarkdoc/releases/download/v{{.Version}}/gomarkdoc_{{.Version}}_{{.GOOS}}_{{.GOARCH}}{{.ArchiveExt}}",
 	))
+	mage = bintool.Must(bintool.New(
+		"mage{{.BinExt}}",
+		"1.14.0",
+		"https://github.com/magefile/mage/releases/download/v{{.Version}}/mage_{{.Version}}_{{.GOOS}}-{{.GOARCH}}{{.ArchiveExt}}",
+		bintool.WithGoBinFolder(),
+		bintool.WithOsSubstitution(mageOsRepl),
+		bintool.WithArchSubstitution(mageArchRepl),
+	))
+
+	mageOsRepl = map[string]string{
+		"darwin":  "macOS",
+		"linux":   "Linux",
+		"windows": "Windows",
+	}
+	mageArchRepl = map[string]string{
+		"amd64": "64bit",
+		"386":   "32bit",
+		"arm":   "ARM",
+		"arm64": "ARM64",
+	}
 )
+
+func Mage() error {
+	return mage.Ensure()
+}
 
 func Lint() error {
 	if err := linter.Ensure(); err != nil {
